4bd082a4af1c284e757e4ad739770313d3821e11
Shivam9701 <shivamkichitthi@gmail.com>
2023-08-31 15:00:03 +0530

    -exercise-detail-(Detail #1)

---
+++ src/components/Detail.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import {Stack,Button,Typography} from '@mui/material';
+import BodyPartImage from '../assets/icons/body-part.png';
+import TargetImage from '../assets/icons/target.png';
+import EquipmentImage from '../assets/icons/equipment.png';
+
+const Detail = ({exerciseDetail}) => {
+
+  const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+  
+  const extraDetail =[
+    {
+        icon:BodyPartImage,
+        name:bodyPart,
+        id:1
+    },
+    {
+        icon : TargetImage,
+        name:target,
+        id:2
+    }
+    ,{
+        icon:EquipmentImage,
+        name:equipment,
+        id:3
+    }
+  ]
+
+  return (
+    <Stack gap="60px" sx={{flexDirection:{lg:'row'},p:'20px',alignItems:'center'}}>
+    
+    <img src={gifUrl} alt="gif" loading="lazy"/>
+
+    <Stack sx= {{gap:{lg:'35px',xs:'20px'}}}>
+
+        <Typography variant="h3">
+            {name}
+        </Typography>
+
+        <Typography sx={{ fontSize: { lg: '24px', xs: '18px' } }} color="#4F4C4C">
+            Exercises keep you strong.{' '}
+            <span style={{ textTransform: 'capitalize' }}>{name}</span> is one
+            of the best <br /> exercises to target your {target}. It will help you improve your{' '}
+            <br /> mood and gain energy.
+        </Typography>
+
+        {extraDetail.map((item) => {
+          return (
+            <Stack key={item.id} direction="row" gap="24px" alignItems='center'>
+              <Button sx={{background:'#fff2db',borderRadius:'50%',width:'100px',height:'100px'}}>
+                    <img src= {item.icon} alt='help' style={{width:'50px',height:'50px'}} />
+              </Button> 
+              <Typography variant='h5' textTransform='capitalize'>
+                {item.name}
+              </Typography>           
+            </Stack>
+          );
+        })} 
+
+    </Stack>
+    </Stack>
+  )
+
+}
+
+export default Detail
--- src/pages/exerciseDetail.jsx
+++ src/pages/exerciseDetail.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React ,{useState,useEffect} from 'react'
+import {useParams} from 'react-router-dom';
+import {Box} from '@mui/material';
+import {exerciseOptions,fetchData} from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
-const exerciseDetail = () => {
+const ExerciseDetail = () => {
+  const [exerciseDetail,setExerciseDetail] = useState({});
+  const {id} = useParams();
+
+  useEffect(() => { 
+     const fetchExerciseData =async ()=>{
+       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com/exercises';
+       const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercise/${id}`,exerciseOptions);
+      
+       setExerciseDetail(exerciseDetailData);
+      }
+      fetchExerciseData();
+  },[id]);
+
   return (
-    <div>exerciseDetail</div>
+    <Box>
+     
+    <Detail exerciseDetail={exerciseDetail}/>
+    <ExerciseVideos />
+    <SimilarExercises />
+    </Box>
   )
 }
 
-export default exerciseDetail
+export default ExerciseDetail;
