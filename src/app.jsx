0019c5d64bd30d8a755004552920f7df24775a1c
Shivam9701 <shivamkichitthi@gmail.com>
2023-09-01 13:59:57 +0530

    -o

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
+
+import './App.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import ExerciseDetail from './pages/ExerciseDetail';
 
-
 import Footer from './components/Footer';
-import './App.css';
+
 
 const App = () => {
   return (
