7782763e7c6ffdcf6050082bc162236cc8772c7c
Shivam9701 <shivamkichitthi@gmail.com>
2023-09-01 12:44:46 +0530

    -final

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -8,14 +8,14 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         Watch <span style={{ color: '#ff2625', textTransform: 'capitalize' }}>{name}</span> exercise videos
       </Typography>
       <Stack sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', sm: '50px' }, justifyContent: 'flex-start', alignItems: 'center', flexWrap: 'wrap' }}>
-        {exerciseVideos?.slice(0, 6).map((item, index) => {
+        {exerciseVideos?.slice(0, 3).map((item, index) => {
           return (
             <a key={index} className='exercise-video'
               href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
               target='_blank' rel='noreferrer'>
               <img src={item.video.thumbnails[0].url} alt={item.video.title} />
-              <Box>
-                <Typography variant="h5" color="#ff0000">
+              <Box sx={{overflow:'hidden',textOverflow:'ellipsis'}}>
+                <Typography variant="h5" color="#ff0000" sx={{whiteSpace:'nowrap',textOverflow:'ellipsis'}}>
                   {item.video.title}</Typography>
                 <Typography variant="h6" color="#000">
                   {item.video.channelName}</Typography>
